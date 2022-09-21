9876fdd358456b271bbdcc32a14a6c4216a19d7a
Asim Mehmood Khan <57537718+AsimKhan2019@users.noreply.github.com>
2022-09-21 16:26:01 +0500

    Updated file to fix linter errors
    Handled snake_case and long length variables

--- decode_morse_code.rb
+++ decode_morse_code.rb
@@ -1,11 +1,11 @@
-@morse_dict = { '.-' => 'A', '-...' => 'B', '---' => 'O', '-..-' => 'X', '..-.' => 'F', '..-' => 'U', '.-..' => 'L', '.-.' => 'R',  '..' => 'I', '.' => 'E', '...' => 'S' }
-
+@morse_dict = { '.-' => 'A', '-...' => 'B', '---' => 'O', '-..-' => 'X', '..-.' => 'F', '..-' => 'U', '.-..' => 'L', '.-.' => 'R',  '..' => 'I', 
+  '.' => 'E', '...' => 'S' }
 def get_single_letter(letter)
-  @singleLetter = @morse_dict[letter]
-  @singleLetter
+  @single_letter = @morse_dict[letter]
+  @single_letter
 end
 
-def decode_theword(word)
+def decode_the_word(word)
   @letters = word.split
   @currentword = ''
   @letters.each do |letter|
@@ -17,7 +17,7 @@ end
 def decode_final_message(message)
   @words = message.split('   ')
   @words.each do |word|
-    decode_theword(word)
+    decode_the_word(word)
   end
 end
 decode_final_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
